0dfb3606218678f3872d63611008a0868b25797c
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2022-07-22 03:32:57 +0530

    balance is showing on nav

--- components/Wallet/Wallet.jsx
+++ components/Wallet/Wallet.jsx
@@ -41,7 +41,7 @@ const Wallet = () => {
     return (
         <button onClick={connectWallet} type="button" className="flex text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">
             <img src="/metamask.svg" className='h-5 w-5 mr-3' />
-            <span>{address ? `${address.slice(0, 5)}` + `...` + `${address.slice(-4, -1)}` : `Connect`}</span>
+            <span>{address ? `${address.slice(0, 5)}` + `...` + `${address.slice(-4, -1)}` + ` | ${balance.slice(0, 5)} ETH` : `Connect`}</span>
         </button>
     )
 }
