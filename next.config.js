8ffecef8c0da3ee6da56c19f3fd39f3d8f53ce86
Gurpreet Singh <lostlegend2701@gmail.com>
2023-04-21 17:36:40 +0530

    fleek build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  exportTrailingSlash: true,
   images: {
     domains: ['ipfs.infura.io', 'th.bing.com', 'images.unsplash.com', 'ipfs.io']
   },
